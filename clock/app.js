a7fc88455c2558a888e20bc2117a2cc199761c12
Alishan89710 <79788347+Alishan89710@users.noreply.github.com>
2021-06-01 01:20:28 +0500

    analog clock

---
+++ clock/app.js
@@ -0,0 +1,13 @@
+setInterval(()=>{
+d = new Date()
+htime = d.getHours();
+mtime = d.getMinutes();
+stime = d.getSeconds();
+hrotation = htime*30 + mtime/2;
+mrotation = 6*mtime;
+srotation = 6*stime;
+
+hourly.style.transform=`rotate(${hrotation}deg)`
+minutes.style.transform=`rotate(${mrotation}deg)`
+seconds.style.transform=`rotate(${srotation}deg)`
+},1000);
