dbe1b3e7b4e782d92e51bdd8f3a7fcbb136cb64a
Alishan89710 <79788347+Alishan89710@users.noreply.github.com>
2021-06-27 00:35:36 +0500

    plasehoder

--- A.T.A/app.js
+++ A.T.A/app.js
@@ -1,11 +1,11 @@
-var  day = prompt("Enter Your Name!") 
+var  day = prompt("Enter Your Name!","Name Please") 
 day = day.toLowerCase();
 dat = day.toUpperCase();
 switch(day) {
-     case "anas khan" :
+     case "anas khan","anas khan lodhi" :
      alert("ya wo insan he jis k birth certificate me father,s name ki jaga chack box ka option laga hua ya jitne chahe apne baap bana le ");
      break;
-     case "talha":
+     case "talha","talha ashraf":
      alert("chal talha ki nasal!");
      break;
      case "alishan" :
