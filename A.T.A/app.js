06207b743ef68785c9b7352c4a1d27d051e51f6a
Alishan89710 <79788347+Alishan89710@users.noreply.github.com>
2021-06-27 00:43:48 +0500

    this correction

--- A.T.A/app.js
+++ A.T.A/app.js
@@ -2,13 +2,13 @@ var  day = prompt("Enter Your Name!")
 day = day.toLowerCase();
 dat = day.toUpperCase();
 switch(day) {
-     case "anas khan" , "anas khan lodhi" :
+     case "anas khan"  :
      alert("ya wo insan he jis k birth certificate me father,s name ki jaga chack box ka option laga hua ya jitne chahe apne baap bana le ");
      break;
-     case "talha" , "talha ashraf":
+     case "talha" :
      alert("chal talha ki nasal!");
      break;
-     case "alishan" , "ali shan" :
+     case "alishan"  :
      alert("ya tera baap he kia!");
      break;
      default :
