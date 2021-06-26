b45a987310cbe59e3438c29606516089b056d5af
Alishan89710 <79788347+Alishan89710@users.noreply.github.com>
2021-06-27 00:03:56 +0500

    new program

---
+++ A.T.A/app.js
@@ -0,0 +1,17 @@
+var  day = prompt("Enter Your Name!") 
+day.toLowerCase();
+day.toUpperCase();
+switch(day) {
+     case "anas khan","ANAS KHAN","Anas khan" :
+     alert("ya wo insan he jis k birth certificate me father,s name ki jaga chack box ka option laga hua ya jitne chahe apne baap bana le ");
+     break;
+     case "Talha","TALHA","talha":
+     alert("chal talha ki nasal!");
+     break;
+     case "Alishan","alishan","ALISHAN" :
+     alert("ya tera baap he kia!");
+     break;
+     default :
+     alert("Gando sahi name likh!");
+     }
+ 
--- clock/app.js
+++ clock/app.js
@@ -7,7 +7,7 @@ hrotation = htime*30 + mtime/2;
 mrotation = 6*mtime;
 srotation = 6*stime;
 
-hourly.style.transform=`rotate(${hrotation}deg)`
-minutes.style.transform=`rotate(${mrotation}deg)`
-seconds.style.transform=`rotate(${srotation}deg)`
-},1000);
+hourly.style.transform =`rotate(${hrotation}deg)`
+minutes.style.transform =`rotate(${mrotation}deg)`
+seconds.style.transform =`rotate(${srotation}deg)`
+},1000)
